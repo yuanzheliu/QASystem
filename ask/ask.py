d4547c984e53208b4d41a969605429a6af5b6df2
yuanzheliu <51001897+yuanzheliu@users.noreply.github.com>
2020-04-08 02:29:55 -0400

    Add files via upload

--- ask/ask.py
+++ ask/ask.py
@@ -1,51 +1,57 @@
-import nltk
-import spacy
-nlp = spacy.load("en_core_web_sm")
+from ask_sentence import ask_sentence
+import sys
+from nltk import sent_tokenize
+import heapq
 
+class model_ask(): 
+    def __init__(self, sentences, num_questions):
+        '''
+        sentences: A list of strings. Each string is one sentence
+        num_questions: Number of questions to generate
+        '''
+        self.num_questions = num_questions
+        self.sentences = sentences
+        self.questions = [] # A list of tuple (score, 'Question', 'Answer'), score is the quality of a question. 
 
-
-
-
-def ask_sentence(text):
-    result = ''
-    t = nltk.word_tokenize(text)
-    doc = nlp(text)
-    if nltk.pos_tag(t)[0][1] in {"NN", "NNS", "NNP", "NNPS"}: 
-                if doc[0].ent_type_ == "PERSON": 
-                    result = "Who "
-                elif doc[0].ent_type_ in {"GPE", "LOC"}: 
-                    result = "Where "
+    def ask_questions(self):
+        number = 1
+        for s in self.sentences: 
+            tmp = ask_sentence(s)
+            if tmp != None: 
+                if number > self.num_questions:
+                    heapq.heappushpop(self.questions,tmp)
                 else: 
-                    result = "What "
-                result += " ".join(t[1:-1]) + "?"
-    elif nltk.pos_tag(t)[0][1] == 'PRP': 
-        if t[0] in {'He', 'She', 'I', 'You'}:   #TODO: "They"?
-            result = "Who "
-        elif t[0] == 'It': 
-            result = 'What '
-        result += " ".join(t[1:-1]) + "?"
-    else:
-        print(nltk.pos_tag(t)[0][1])
-    return result
+                    heapq.heappush(self.questions,tmp)
+                    number += 1
+        return self.questions
 
 
-text = "Pittsburgh has no nCov."
-print(text, "============>",ask_sentence(text))
-text = "Thomas is writing code."
-print(text, "============>",ask_sentence(text))
-text = "He is writing code."
-print(text, "============>",ask_sentence(text))
-text = "It has no effect on the run."
-print(text, "============>",ask_sentence(text))
-text = "CNN performs better than MLP on image classification."
-print(text, "============>",ask_sentence(text))
-text = "Apple tasts better."
-print(text, "============>",ask_sentence(text))
-text = "Apples are better than peaches."
-print(text, "============>",ask_sentence(text))
-# text = "The house is on the run."   #TODO
-# print(text, "============>",ask_sentence(text))
-# doc = nlp(text)
-# print(nltk.pos_tag(nltk.word_tokenize(text)))
-# for token in doc: 
-#     print(token.text, token.ent_type_)
+if __name__ == "__main__":
+    file_path, num_questions  = "", 0
+    try:
+        file_path = sys.argv[1]
+        num_questions = int(sys.argv[2])
+    except:
+        print('usage: python ./ask.py <path to file> <number of questions>')
+    
+    model = None
+    with open(file_path, 'r', encoding= 'utf-8') as f: 
+        article = f.read()
+        article_sentences = sent_tokenize(article)
+        model = model_ask(article_sentences, num_questions)
+
+    result = model.ask_questions()
+    questions = [q[1] for q in result]
+    answers = [q[2] for q in result]
+    print('***************************')
+    i = 1
+    for q in questions:
+        print("Q{} {}".format(i,q))
+        i += 1    
+    i = 1
+    print('***************************')
+    for a in answers:
+        print("A{} {}".format(i,a))
+        i += 1
+    print('***************************')
+
