4631bb712b33f5ae6667f3e64ffa6104c85a9e63
SifanStephanie <sifanl@andrew.cmu.edu>
2020-04-19 11:33:22 -0400

    finish wh question

--- ans/answer.py
+++ ans/answer.py
@@ -1,78 +1,13 @@
 import sys
-from nltk import ne_chunk, word_tokenize, pos_tag
-from nltk.tree import Tree
+
+from nltk import ne_chunk, pos_tag, word_tokenize
 from nltk.tag import StanfordNERTagger
+from nltk.tree import Tree
+
 from question_type import QuestionType
 from top_k_sentences import ArticleQuestions
+from wh import WHQ
 from yes_no import yes_no_answer
-
-
-import sys
-from question_type import *
-from top_k_sentences import *
-from nltk import ne_chunk
-from nltk.tree import Tree
-from nltk.tag import StanfordNERTagger
-
-
-class WHQ():
-    def __init__(self, question, qtype, top_sentences):
-        self.question = question
-        self.qtype = qtype
-        self.top_sentences = top_sentences
-        self.ner = StanfordNERTagger('english.muc.7class.distsim.crf.ser.gz',
-                                     'stanford-ner.jar')
-
-    def find_answers(self):
-        '''
-            Find the correct phrase in a sentence's ner dictionary to answer
-            the question of type 'type'
-            Output:
-            - answer: a string phrase to answer the question
-        '''
-        answer = "Sorry, I don't know"
-        sentences_ner = self.sentence_ner(self.top_sentences)
-        answer = self.answer_for_type(self.qtype, sentences_ner)
-        return answer
-
-    def sentence_ner(self, sentences):
-        '''
-            Generate a dictionary which maps each sentence to its NER dictionary
-            which consists of (type, [phrases]) mappings.
-            Input:
-            - sentences: a list of top k sentences as strings
-            Output:
-            - sentences_ner: a dictionary of (sentence, {type: [phrases]})
-        '''
-        sentences_ner = {}
-        for sentence in sentences:
-            ner_dict = {}
-            dependency_tree = ne_chunk(pos_tag(word_tokenize(sentence)))
-            for node in dependency_tree:
-                if type(node) == Tree:
-                    phrase = ' '.join([token for token, _ in node.leaves()])
-                    ner_dict[node.label()] = ner_dict.get(node.label(), []) + [phrase]
-            sentences_ner[sentence] = ner_dict
-        return sentences_ner
-
-    def answer_for_type(self, type, sentences_ner):
-        '''
-            Find the correct phrase in a sentence's ner dictionary to answer
-            the question of type 'type'
-            Input:
-            - type: the question's type as a string
-            - sentences_ner: a dictionary mapping top k sentences to their NER
-              dictionaries
-            Output:
-            - answer: a string phrase to answer the question
-        '''
-        answer = "Sorry, I don't know."
-        for sentence, ner_dict in sentences_ner.items():
-            for (ner, item) in ner_dict.items():
-                if type == ner or (type == 'ORGANIZATION' and ner == 'GPE'):
-                    answer = item[0]  # TODO: need to pick the right phrase!
-        return answer
-
 
 if __name__ == '__main__':
     article_file, questions_file = sys.argv[1:]
--- ans/question_type.py
+++ ans/question_type.py
@@ -4,7 +4,8 @@ and avoid Stanford tokenizer/segmenter from nltk.tokenize.
 They are currently deprecated and will be removed in due time.
 '''
 from nltk import word_tokenize
-from nltk.parse import CoreNLPParser # use this parser
+from nltk.parse import CoreNLPParser  # use this parser
+
 
 class QuestionType():
     def __init__(self, questions):
@@ -23,37 +24,61 @@ class QuestionType():
             - questions_type_dict: a dictionary of {question: type}
         '''
         # Lexical Parser
-        parser = CoreNLPParser(url='http://localhost:9000')
+        #parser = CoreNLPParser(url='http://localhost:9000')
         questions_type_dict = {}
+        '''
         for question in questions:
             parse_question = list(parser.raw_parse(question))
             parse_tree = parse_question[0][0]
             questions_type_dict[question] = self.type(parse_tree,question.lower())
+        '''
+        for question in questions:
+            questions_type_dict[question] = self.type(question.lower())
         return questions_type_dict
     
-    def get_wh_type(self,parse_tree,question):
-        return 'PERSON'
-
+    def get_wh_type(self,tokens):
+        question_type = 'WHAT'
+        multi_answer = ['are','were','they','them','our','we']
+        check_len = 5
+        if len(tokens)<5:
+            check_len = len(tokens)
+        if 'who' == tokens[0] or 'whom' == tokens[0] or 'whose' == tokens[0]:
+            question_type = 'PERSON'
+        elif 'when' == tokens[0]:
+            question_type = 'TIME'
+        elif 'where' == tokens[0]:
+            question_type = 'LOCATION'
+        elif 'why' == tokens[0]:
+            question_type = 'WHY'
+        elif 'how' == tokens[0]:
+            question_type = 'HOW'
+        
+        # multi-answers question
+        for t in tokens[:check_len]:
+            if t in multi_answer:
+                question_type = 'MULTI'
+                break
+        return question_type
+    '''  
     def type(self, parse_tree, question):
-        '''
-            Extract the indicator words from a tokenized and tagged question,
-            assign, type ('PERSON', 'GPE', 'TIME', 'REASON', 'ORGANIZATION',
-            'WAY', 'unknown') to it.
-
-            Input:
-            - token_pos: a tokenized and tagged sentence as a list of (token,
-              tag) tuples
-
-            Output:
-            - type: type of indicator word
-        '''
-        type = 'unknown'
+        question_type = 'unknown'
         yesno = ["is", "are", "was", "were", "does", "did", "have", "has",
                  "had", "can", "could", "will", "would"]
         tokens = word_tokenize(question)
 
         if parse_tree._label == 'SBARQ' or question.startswith('wh') or question.startswith('ho'):
-            type = self.get_wh_type(parse_tree,question)
+            question_type = self.get_wh_type(parse_tree,tokens)
         elif parse_tree._label == 'SQ' or tokens[0] in yesno:
-            type = 'YESNO'
-        return type
+            question_type = 'YESNO'
+        return question_type
+    '''
+    def type(self,question):
+        question_type = 'WHAT'
+        tokens = word_tokenize(question)
+        yesno = ["is", "are", "was", "were", "does", "did", "have", "has",
+                 "had", "can", "could", "will", "would"]
+        if question.startswith('wh') or question.startswith('ho'):
+            question_type = self.get_wh_type(tokens)
+        elif tokens[0] in yesno:
+            question_type ='YESNO'
+        return question_type
