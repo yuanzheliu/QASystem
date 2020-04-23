a47dc5d8dc11a4f7892dfd9ef00c335cd62e9ef0
yuanzheliu <51001897+yuanzheliu@users.noreply.github.com>
2020-04-23 00:05:32 -0400

    Add files via upload

--- ask/ask.py
+++ ask/ask.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from ask_sentence import *
+from ask_sentence import ask_sentence
 import sys
 from nltk import sent_tokenize
 import heapq
@@ -13,19 +13,71 @@ class model_ask():
         '''
         self.num_questions = num_questions
         self.sentences = sentences
-        self.questions = [] # A list of tuple (score, 'Question', 'Answer'), score is the quality of a question.
 
-    def ask_questions(self):
-        number = 1
+        self.who = []
+        self.what = []
+        self.when = []
+        self.where = []
+        self.why = []
+        self.yesno = []
+    
+    def add_question(self, queue ,question): 
+        if len(queue) >= self.num_questions: 
+            heapq.heappushpop(queue, question)
+        else: 
+            heapq.heappush(self.questions,question)
+
+    def generate_question(self):
         for s in self.sentences: 
-            tmp = ask_sentence(s)
-            if tmp != None: 
-                if number > self.num_questions:
-                    heapq.heappushpop(self.questions,tmp)
-                else: 
-                    heapq.heappush(self.questions,tmp)
-                    number += 1
-        return self.questions
+            qs = ask_sentence(s)
+            if len(qs) == 0: 
+                continue 
+            else: 
+                for (score,question,answer,q_type) in qs:
+                    if q_type == 'who': 
+                        add_question(self.who, (score,question,answer))
+                    elif q_type == 'what': 
+                        add_question(self.what, (score,question,answer))
+                    elif q_type == 'when': 
+                        add_question(self.when, (score,question,answer))
+                    elif q_type == 'where': 
+                        add_question(self.where, (score,question,answer))
+                    elif q_type == 'why': 
+                        add_question(self.why, (score,question,answer))
+                    elif q_type == 'yesno': 
+                        add_question(self.yesno, (score,question,answer))
+
+    def ask_question(self):
+        result = []
+        i = -1
+        while len(result) < self.num_questions:
+            i += 1
+            if i % 6 == 0:
+                if len(self.who) != 0:
+                    result += heapq.heappop(self.who)
+                    continue
+            elif i % 6 == 1:
+                if len(self.what) != 0:
+                    result += heapq.heappop(self.what)
+                    continue
+            elif i % 6 == 2:
+                if len(self.when) != 0:
+                    result += heapq.heappop(self.when)
+                    continue
+            elif i % 6 == 3:
+                if len(self.where) != 0:
+                    result += heapq.heappop(self.where)
+                    continue
+            elif i % 6 == 4:
+                if len(self.why) != 0:
+                    result += heapq.heappop(self.why)
+                    continue
+            elif i % 6 == 5:
+                if len(self.yesno) != 0:
+                    result += heapq.heappop(self.yesno)
+                    continue
+        return result
+                
 
 if __name__ == "__main__":
     file_path, num_questions  = "", 0
@@ -40,15 +92,13 @@ if __name__ == "__main__":
         article = f.read()
         article_sentences = sent_tokenize(article)
         model = model_ask(article_sentences, num_questions)
-
+    
+    model.generate_question()
     result = model.ask_questions()
     questions = [q[1] for q in result]
     answers = [q[2] for q in result]
-
-    for i,q in enumerate(questions):
-        a = "Q{} {}\n".format(i+1,q)
-        sys.stdout.buffer.write(a.encode('utf8'))
-        sys.stdout.buffer.write('-----'.encode('utf8'))
-        sys.stdout.buffer.write(answers[i].encode('utf8'))
-        sys.stdout.buffer.write('\n'.encode('utf8'))
+    i = 1
+    for q in questions:
+        print("Q{} {}".format(i,q))
+        i += 1    
 
