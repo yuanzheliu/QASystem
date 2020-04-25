f85d9e53a667d0999c12c5e7ff9efef3df41beda
Doris Duan <yanjiad@andrew.cmu.edu>
2020-04-25 02:24:32 -0400

    noun_chunk

--- ans/parse.py
+++ ans/parse.py
@@ -1,53 +1,29 @@
 import spacy
-from nltk import ne_chunk, pos_tag, word_tokenize
-from nltk.tree import Tree
 
 nlp = spacy.load('en_core_web_sm')
 
 
-def get_who_answer(question, sentence):
-    answer = None
+def extract_wh_answer(question, qtype, sentence):
+    sentence_parsed = nlp(sentence)
+    if qtype == 'TIME':
+        for token in sentence_parsed:
+            if token.tag_ == 'CD'and token.text not in question:
+                if token.dep_ == 'nummod':
+                    return token.text + ' ' + token.head.text
+                return token.text
+        return sentence
+
     question_parsed = nlp(question)
-    sentence_parsed = nlp(sentence)
-    sentence_chunked = ne_chunk(pos_tag(word_tokenize(sentence)))
-    dictionary = dict(pos_tag(word_tokenize(sentence)))
-    root_verb = ''
-    for token in question_parsed:
-        if token.dep_ == 'ROOT':
-            root_verb = token.text
+    question_noun_chunk = []
+    sentence_noun_chunk = []
+    for chunk in question_parsed.noun_chunks:
+        question_noun_chunk.append(chunk.text)
+    for chunk in sentence_parsed.noun_chunks:
+        sentence_noun_chunk.append(chunk.text)
 
-    for token in sentence_parsed:
-        children = [child.text for child in token.children]
-        if root_verb in children:
-            for child in token.children:
-                if child.text in dictionary and dictionary[child.text] == 'NNP':
-                    return child.text
-    if answer is None:
-        for node in sentence_chunked:
-            if type(node) == Tree and node.label() == 'PERSON':
-                phrase = ' '.join([token for token, _ in node.leaves()])
-                if phrase not in question:
-                    return phrase
-    return sentence
-
-
-def get_where_answer(question, sentence):
-    sentence_parsed = ne_chunk(pos_tag(word_tokenize(sentence)))
-    for node in sentence_parsed:
-        if type(node) == Tree and node.label() == 'GPE':
-            phrase = ' '.join([token for token, _ in node.leaves()])
-            if phrase not in question:
-                return phrase
-    return sentence
-
-
-def get_when_answer(question, sentence):
-    # for token in sentence_parsed:
-    #     print(token.text, token.tag_, token.head.text, token.dep_)
-    sentence_parsed = nlp(sentence)
-    for token in sentence_parsed:
-        if token.tag_ == 'CD'and token.text not in question:
-            if token.dep_ == 'nummod':
-                return token.text + ' ' + token.head.text
-            return token.text
+    if qtype == 'PERSON':
+        sentence_noun_chunk = sentence_noun_chunk[::-1]
+    for noun_chunk in sentence_noun_chunk:
+        if noun_chunk not in question_noun_chunk:
+            return noun_chunk
     return sentence
